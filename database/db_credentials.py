cf28ee31019f61371241bd2629b678da0335b025
Kavya Kolavasi <135289399+KavyaKolavasi1@users.noreply.github.com>
2024-08-16 10:36:03 -0500

    Add files via upload

---
+++ database/db_credentials.py
@@ -0,0 +1,11 @@
+# To actually have your app use this file, you need to RENAME the file to db_credentials.py
+# You will find details about your CS340 database credentials on Canvas.
+
+# the following will be used by the webapp.py to interact with the database
+# You can also use environment variables
+
+
+# host = 'classmysql.engr.oregonstate.edu'      # MUST BE THIS
+# user = '<your-cs-340-db-username-here>'       # don't forget the cs340_ prefix
+# passwd = '<your-password-here>'               # should only be 4 digits if default
+# db = '<name-of-database-on-osu-server>'                                  
